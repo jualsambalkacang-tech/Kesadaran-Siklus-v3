ae60c052d59f7bf9fb192bfcea6760bf181e3415
jualsambalkacang-tech <jualsambalkacang@gmail.com>
2026-08-04 19:04:05 +0700

    Update app.js

--- app.js
+++ app.js
@@ -389,6 +389,60 @@ function showHourChart(){
     drawHourChart(hours);
 
 }
+function drawHourChart(hours){
+
+    const ctx = document
+        .getElementById("hourChart")
+        .getContext("2d");
+
+    if(hourChart){
+
+        hourChart.destroy();
+
+    }
+
+    hourChart = new Chart(ctx,{
+
+        type:"bar",
+
+        data:{
+
+            labels:[
+                "0","1","2","3","4","5",
+                "6","7","8","9","10","11",
+                "12","13","14","15","16","17",
+                "18","19","20","21","22","23"
+            ],
+
+            datasets:[{
+
+                label:"BAK",
+
+                data:hours
+
+            }]
+
+        },
+
+        options:{
+
+            responsive:true,
+
+            plugins:{
+
+                legend:{
+
+                    display:false
+
+                }
+
+            }
+
+        }
+
+    });
+
+}
 document.addEventListener("DOMContentLoaded",()=>{
 
 renderPeeHistory();
@@ -398,6 +452,8 @@ renderPeeChart();
 showWeeklyTrend();
 
 showMonthlyTrend();
+
+showHourChart();
   
 updateDashboard();
 
